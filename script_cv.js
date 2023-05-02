89276b6ba5e03ebfc2763937e58a70e2b56e4fdb
ArmerZ <50855686+armernuntiya@users.noreply.github.com>
2023-05-03 06:05:45 +0700

    .

--- script_cv.js
+++ script_cv.js
@@ -1,5 +1,5 @@
 // TODO #4.0: Change this IP address to EC2 instance public IP address when you are going to deploy this web application
-const backendIPAddress = "127.0.0.1:3000";
+const backendIPAddress = " 52.6.65.2:3000";
 var cv_cid=[]
 var item2=[]
 var item3=[]
